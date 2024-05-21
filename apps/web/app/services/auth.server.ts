602463c433843c1b360b4197a4c6b55cefce3c97
Gimel Dick <gimeldick@gmail.com>
2024-05-21 17:28:55 -0400

    quick auth fix

--- apps/web/app/services/auth.server.ts
+++ apps/web/app/services/auth.server.ts
@@ -4,7 +4,7 @@ import { options } from "./auth";
 
 export const authClient = createClient({
   //Note: when developing locally you will need to set tls  security to insecure, because the dev server uses  self-signed certificates which will cause api calls with the fetch api to fail.
-  tlsSecurity: "insecure",
+  tlsSecurity: process.env.NODE_ENV === "production" ? "insecure" : "default",
 });
 
 export const auth = createServerAuth(authClient, options);
